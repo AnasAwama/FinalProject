9e1dda5dddc3235991e44bb47c2b437466561555
AnasAwama <90322659+AnasAwama@users.noreply.github.com>
2024-01-19 15:09:31 +0200

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Weather from './Weather';
-//import Weather2 from './weather2';
 
 function App() {
   return (
     <Weather/>
-    //<Weather2/>
+    
   );
 }
 
