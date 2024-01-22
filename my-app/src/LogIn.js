1daf2a5bb919937679a04c140af5c8f4edf3860a
AnasAwama <90322659+AnasAwama@users.noreply.github.com>
2024-01-22 18:34:10 +0200

    Update LogIn.js

--- my-app/src/LogIn.js
+++ my-app/src/LogIn.js
@@ -1,5 +1,6 @@
 import React from "react"
-
+import { BrowserRouter as Router, Routes, Route ,Link} from 'react-router-dom';
+import './Login.css';
 class LogIn extends React.Component{
 
 
@@ -7,14 +8,32 @@ class LogIn extends React.Component{
     render(){
 
         return(
-            <body>
-                <div>
-                    <form action="" method="post">
-                        <h1>Log In Page</h1>
-                        <input type="Text" name="email" placeholder="Enter Your Email"/>
-                        <input type="Password" name="pass" placeholder="Enter Your Password"/>
-                        <input type="Submit" name="submit"/>
+            <body className="imageLog">
+                <nav class="navbar navbar-expand-lg ">
+                    <div class="container-fluid ">
+                    <Link to="/Weather" class="navbar-brand " >Weather App</Link>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            {/* add item */}
+                            </li>
+                        </ul>
+                        </div>
+                    </div>
+                </nav>
+            <div className="margtop">
+                <div className="BorderLog">
+                    
+                    <form action="http://localhost:9000/logIn" method="post">
+                        <h1 className="Logh1">Log In Page</h1>
+                        <input className="formMid" type="Text" name="email" placeholder="Enter Your Email"/><br/>
+                        <input className="formMid" type="Password" name="pass" placeholder="Enter Your Password"/><br/>
+                        <input className="formbut" type="Submit" name="submit"/>
                     </form>
+                </div>
                 </div>
             </body>
         )
