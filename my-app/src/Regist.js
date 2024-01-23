fdd28226d91b85d5343f4618d142c43a5d0d21a2
AnasAwama <90322659+AnasAwama@users.noreply.github.com>
2024-01-23 18:27:25 +0200

    Update Regist.js

--- my-app/src/Regist.js
+++ my-app/src/Regist.js
@@ -1,54 +1,65 @@
-import React ,{useState}from "react"
-import { BrowserRouter as Router, Routes, Route ,Link,withRouter  } from 'react-router-dom';
-
+import React ,{Component}from "react"
+import { BrowserRouter as Router, Routes, Route ,Link,useNavigate} from 'react-router-dom';
 import './Regist.css';
-class Regist extends React.Component{
 
-    constructor(props) {
+class Regist extends React.Component{
+    constructor(props){
         super(props);
-        this.state = {};
+        this.state={
+            name:"",
+            email:"",
+            password:"",
+            gender:"",
+            age:0,
+            flag:0,
+        };
+        this.handleSubmit=this.handleSubmit.bind(this);
     }
 
-    handleSubmit = async (e) => {
+    handleAgeChange = (e) => {
+        this.setState({ age: parseInt(e.target.value) });
+    }
+
+    handleSubmit(e){
         e.preventDefault();
-
-        try {
-            const response = await fetch('http://localhost:9000/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: new URLSearchParams({
-                    name: this.state.name,
-                    email: this.state.email,
-                    password: this.state.password,
-                    confirmPassword: this.state.confirmPassword,
-                    dob: this.state.dob,
-                    gender: this.state.gender,
-                }),
-            });
-
-            const responseData = await response.json();
-
-            if (response.ok) {
-                console.log(responseData.message);
-                this.props.history.push('/Weather');
-            } else {
-                alert(responseData.error);
+        const {name,email,password,age,gender} = this.state;
+        console.log("Form Values: ",name,email,password,gender,age);
+        fetch("http://localhost:9000/regist", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password,
+                age,
+                gender,
+            }),
+            
+        })
+        .then((res) => {
+            console.log("Res result: " ,res)
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
             }
-        } catch (error) {
-            console.error('Error registering user:', error);
-            alert('Internal server error');
-        }
-    };
-
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data, "userRegister");
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+                }
     render(){
 
         return(
             <body className="imageReg">
             <nav class="navbar navbar-expand-lg ">
                 <div class="container-fluid ">
-                <Link to="/Weather" class="navbar-brand " >Weather App</Link>
+                <Link to="/" class="navbar-brand " >Weather App</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
@@ -62,23 +73,32 @@ class Regist extends React.Component{
                 </div>
             </nav>
                 <div className="BorderReg">
-                    <form action="http://localhost:9000/register" onSubmit={this.handleSubmit} method="post">
+                    <form onSubmit={this.handleSubmit} method="post">
                         <h1 className="Regh1">Registration Page</h1>
                         <div class="formInputGroup">
-                            <input className="formInput" type="text" name="name" placeholder="Full Name" required/>
-                            <input className="formInput" type="email" name="email" placeholder="Email Address" required/>
+                            <input className="formInput" type="text" name="name" onChange={e=>this.setState({name:e.target.value})} placeholder="Full Name" required/>
+                            <input className="formInput" type="email" name="email" onChange={e=>this.setState({email:e.target.value})} placeholder="Email Address" required/>
                         </div><br/>
                         <div class="formInputGroup">
-                            <input className="formInput" type="password" name="password" placeholder="Create Password" required/>
+                            <input className="formInput" type="password" name="password" onChange={e=>this.setState({password:e.target.value})} placeholder="Create Password" required/>
                             <input className="formInput" type="password" name="confirmPassword" placeholder="Confirm Password" required/>
                         </div><br/>
-                        <label>Date of Birth:</label>
-                        <input className="formInputDate" type="date" name="dob" required/><br/>
+                        <label>Age: </label>
+                        <input
+                                type="range"
+                                className="formInput"
+                                min="0"
+                                max="100"
+                                value={this.state.age}
+                                name="age"
+                                onChange={this.handleAgeChange}
+                            />
+                            <output>{this.state.age}</output><br/><br/>
                         <label>Gender:</label>
                         <div className="formRadioGroup">
-                            <input type="radio" name="gender" value="male" required/> Male
-                            <input type="radio" name="gender" value="female" required/> Female
-                            <input type="radio" name="gender" value="other" required/> Other
+                            <input type="radio" name="gender" onChange={e=>this.setState({gender:e.target.value})} value="male" required/> Male
+                            <input type="radio" name="gender" onChange={e=>this.setState({gender:e.target.value})} value="female" required/> Female
+                            <input type="radio" name="gender" onChange={e=>this.setState({gender:e.target.value})} value="other" required/> Other
                         </div><br/>
                         <input className="formbutReg" type="submit" value="Sign Up"/>
                     </form>
