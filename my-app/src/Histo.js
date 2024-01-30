ed75d7e17437ce1dfa65bfd3fc27640f2c48cac6
AnasAwama <90322659+AnasAwama@users.noreply.github.com>
2024-01-30 17:10:58 +0300

    Update Histo.js

--- my-app/src/Histo.js
+++ my-app/src/Histo.js
@@ -8,6 +8,8 @@ class Histo extends React.Component{
         this.state={
             username:"",
             getHistory:[],
+            currentPage: 1,
+            itemsPerPage: 18,
         };
         
     }
@@ -84,16 +86,39 @@ class Histo extends React.Component{
         console.error("Error during logout:", error);
         }
     }
+    clearHistory=async()=>{
+        try {
+            const response = await fetch("http://localhost:9000/clearHistory", {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({ name: this.state.username }),
+            });
+            if (response.ok) {
+                console.log("History cleared successfully");
+                this.getHistoryValue();
+            } else {
+                console.error("Failed to clear history");
+            }
+        } catch (error) {
+            console.error("Error during clearing history:", error);
+            }
+        };
     
     
     render(){
+        const indexOfLastItem = this.state.currentPage * this.state.itemsPerPage;
+        const indexOfFirstItem = indexOfLastItem - this.state.itemsPerPage;
+        const currentItems = this.state.getHistory.slice(indexOfFirstItem, indexOfLastItem);
 
         return(
             <body className="imageHist">
             
             <nav class="navbar navbar-expand-lg ">
                 <div class="container-fluid ">
-                <Link to="/" class="navbar-brand " >Weather App</Link>
+                <Link to="/" style={{fontSize:"xx-Large"}} class="fontFamily navbar-brand " >Weather App</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
@@ -106,53 +131,58 @@ class Histo extends React.Component{
                     {this.state.username ? (
                     <div className="action">
                         <div className="profile" onClick={()=>this.menuToggle()}>
-                            <p class="Text navbar-text">Welcome, {this.state.username}</p>
+                            <p style={{fontSize:"x-Large"}} class="fontFamily Text navbar-text">Welcome, {this.state.username}</p>
                         </div>
                         <div className="menu">
                         <ul>
                             <li>
-                                <img className="img2" src={require("./image/edit-profile.png")}/><Link to="/Edit">Edit Profile</Link>
+                                <img className="img2" src={require("./image/edit-profile.png")}/><Link to="/Edit" style={{fontSize:"Large"}} className="fontFamily">Edit Profile</Link>
                             </li>
                             <li>
-                            <img className="img2" src={require("./image/logout.png")} /><input type="button" className="logoutButton" value="Logout" onClick={this.handleLogout}/>
+                            <img className="img2" src={require("./image/logout.png")} /><input type="button" style={{fontSize:"Large"}} className="fontFamily logoutButton" value="Logout" onClick={this.handleLogout}/>
                             </li>
                         </ul>
                         </div>
                     </div>
             ) : (
                 <>
-                    <Link to="/LogIn" type="button" class="btn btn-outline-dark Lright">Log-In</Link>
-                    <Link to="/Regist" type="button" class="btn btn-outline-light Sright">Sign-Up</Link>
+                    <Link to="/LogIn" type="button" style={{fontFamily:"poor story", fontSize:"large",fontWeight:"500"}} class="btn btn-outline-dark Lright">Log-In</Link>
+                    <Link to="/Regist" type="button" style={{fontFamily:"poor story", fontSize:"large",fontWeight:"500"}} class="btn btn-outline-light Sright">Sign-Up</Link>
                 </>
             )}
                     </div>
                 </div>
             </nav>
-
             <div className="CentHist">
                 <div className="BorderHist">
                 <div className="txtPosition">User history</div>
-                <table>
-                    <tbody>
-                        <tr>
-                        <td>
-                            <div className="textHist">
-                            <ol className="history-list">
-                                {this.state.getHistory.map((entry, index) => (
-                                <li key={index} className="text-marginHist">
-                                <span className="history-number">{index + 1}.</span>
-                                    {entry}
-                                </li>
-                                ))}
-                            </ol>
-                            </div>
-                        </td>
-                        <td>
-                            {/* Second column content goes here */}
-                        </td>
-                        </tr>
-                    </tbody>
-                    </table>
+                    <div className="textHist">
+                    <ol className="history-list">
+                    {currentItems.map((entry, index) => (
+                        <li key={index} className="text-marginHist">
+                            <span className="history-number">{indexOfFirstItem + index + 1}.</span>
+                            City: {entry.city}, Temperature: {entry.temperature}, Timestamp: {entry.timestamp}
+                        </li>
+                        ))}
+                    </ol>
+                    </div>
+                    
+                    <div className="pagination">
+                    {Array.from({ length: Math.ceil(this.state.getHistory.length / this.state.itemsPerPage) }).map((_, i) => (
+                        <Link
+                        key={i}
+                        to="/Histo"
+                        onClick={() => this.setState({ currentPage: i + 1 })}
+                        className={this.state.currentPage === i + 1 ? "active" : ""}
+                        >
+                        {i + 1}
+                        </Link>
+                    ))}
+                    
+                    </div>
+                    <form onSubmit={this.clearHistory}>
+                        <input className="btn btn-light formbutHisr" type="submit" value="Clear History"/>
+                    </form>
                 </div>
             </div>
             </body>
